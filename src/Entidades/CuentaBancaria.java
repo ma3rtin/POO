4644de8cee3b78fa35ec2d81a8792c86c588152f
Martín <119079697+ma3rtin@users.noreply.github.com>
2023-09-11 14:17:43 -0300

    Update CuentaBancaria.java

--- src/Entidades/CuentaBancaria.java
+++ src/Entidades/CuentaBancaria.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Entidades;
 
-/**
- *
- * @author mutuv
- */
 public class CuentaBancaria {
     private int numeroCuenta;
     private long dniCliente;
