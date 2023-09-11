10ec39227c5724c3f1a779e20bfc10fdd750a3d2
Martín <119079697+ma3rtin@users.noreply.github.com>
2023-09-11 14:19:02 -0300

    Update PersonaServicio.java

--- src/Servicios/PersonaServicio.java
+++ src/Servicios/PersonaServicio.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Servicios;
 
 import Entidades.Persona;
 import java.util.Scanner;
 
-/**
- *
- * @author mutuv
- */
 public class PersonaServicio {
     Persona p1 = new Persona();
     Scanner sc=new Scanner(System.in);
