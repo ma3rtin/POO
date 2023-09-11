1f8609b417992bac239bac3e26aaa1eae9f98cc4
Martín <119079697+ma3rtin@users.noreply.github.com>
2023-09-11 14:18:47 -0300

    Update CuentaBancariaServicio.java

--- src/Servicios/CuentaBancariaServicio.java
+++ src/Servicios/CuentaBancariaServicio.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Servicios;
 
 import Entidades.CuentaBancaria;
 import java.util.Random;
 import java.util.Scanner;
-
-/**
- *
- * @author mutuv
- */
+/
 public class CuentaBancariaServicio {
 
     Scanner sc = new Scanner(System.in);
